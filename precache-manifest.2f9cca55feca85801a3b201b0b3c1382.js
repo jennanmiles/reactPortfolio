667bd69335f946eada99cc85b3122a99067ea990
jennanmiles <jennanmiles@gmail.com>
2020-04-05 14:41:12 -0700

    Updates

---
+++ precache-manifest.2f9cca55feca85801a3b201b0b3c1382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94314ec81d5b3d8b3356ae0898c3ab2d",
+    "url": "/reactPortfolio/index.html"
+  },
+  {
+    "revision": "72c0b311c806b77f2a10",
+    "url": "/reactPortfolio/static/css/main.7f6e5a65.chunk.css"
+  },
+  {
+    "revision": "99e3dcd1ea34e2e652c4",
+    "url": "/reactPortfolio/static/js/2.a0a1804b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactPortfolio/static/js/2.a0a1804b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c0b311c806b77f2a10",
+    "url": "/reactPortfolio/static/js/main.6bd77521.chunk.js"
+  },
+  {
+    "revision": "85d7aa71eb39403fecbb",
+    "url": "/reactPortfolio/static/js/runtime-main.b25a74c4.js"
+  }
+]);
